2cd42db56b06d0b8a0db883eb838f31e2d21fcc0
Gus Cunningham <67352677+cunninghamge@users.noreply.github.com>
2021-05-13 14:53:20 -0600

    add player test with setup and constructor test

---
+++ turn_test.go
@@ -0,0 +1,48 @@
+package main
+
+import (
+	"reflect"
+	"testing"
+)
+
+func TestTurn(t *testing.T) {
+	card1 := Card{"heart", "Jack", 11}
+	card2 := Card{"heart", "10", 10}
+	card3 := Card{"heart", "9", 9}
+	card4 := Card{"diamond", "Jack", 11}
+	card5 := Card{"heart", "8", 8}
+	card6 := Card{"diamond", "Queen", 12}
+	card7 := Card{"heart", "3", 3}
+	card8 := Card{"diamond", "2", 2}
+
+	deck1 := &Deck{
+		[]Card{card1, card2, card5, card8},
+	}
+	deck2 := &Deck{
+		[]Card{card3, card4, card6, card7},
+	}
+
+	player1 := Player{"Megan", deck1}
+	player2 := Player{"Aurora", deck2}
+
+	t.Run("attributes", func(t *testing.T) {
+		turn := Turn{player1, player2}
+
+		if turn.Player1 != player1 {
+			t.Errorf("got %v want %v for player1", turn.Player1, player1)
+		}
+
+		if turn.Player2 != player2 {
+			t.Errorf("got %v want %v for player2", turn.Player2, player2)
+		}
+
+		if !reflect.DeepEqual(turn.SpoilsOfWar, []Card{}) {
+			t.Errorf("got %v want %v for spoils of war", turn.SpoilsOfWar, []Card{})
+		}
+	})
+	// player1, player2, spoilsOfWar
+	// type: basic, war, or mutuallyAssuredDestruction
+	// winner
+	// pile cards
+	// award spoils
+}
