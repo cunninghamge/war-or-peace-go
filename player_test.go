0b52825cd6abb5e1f2c1ab50db9d14bb12c95479
Gus Cunningham <67352677+cunninghamge@users.noreply.github.com>
2021-09-04 23:25:10 -0600

    refactor player_test

--- player_test.go
+++ player_test.go
@@ -1,39 +1,21 @@
 package main
 
 import (
-	"reflect"
 	"testing"
 )
 
-func TestPlayer(t *testing.T) {
-	card1 := Card{"diamond", "Queen", 12}
-	card2 := Card{"spade", "3", 3}
-	card3 := Card{"heart", "Ace", 14}
-	deck := &Deck{
-		[]Card{card1, card2, card3},
-	}
+func TestHasLost(t *testing.T) {
+	deck := &Deck{testCards}
 	player := Player{"Clarissa", deck}
 
-	t.Run("attributes", func(t *testing.T) {
-		if player.Name != "Clarissa" {
-			t.Errorf("got %q want %q for player name", player.Name, "Clarissa")
+	for i := 0; i < 3; i++ {
+		if player.HasLost() {
+			t.Errorf("player has lost but still has cards")
 		}
+		player.Deck.RemoveCard()
+	}
 
-		if !reflect.DeepEqual(player.Deck, deck) {
-			t.Errorf("got %v, want %v for player deck", player.Deck, deck)
-		}
-	})
-
-	t.Run("player loses when they run out of cards", func(t *testing.T) {
-		for i := 0; i < 3; i++ {
-			if player.HasLost() {
-				t.Errorf("player has lost but still has cards")
-			}
-			player.Deck.RemoveCard()
-		}
-
-		if !player.HasLost() {
-			t.Errorf("player has not lost but has no cards")
-		}
-	})
+	if !player.HasLost() {
+		t.Errorf("player has not lost but has no cards")
+	}
 }
