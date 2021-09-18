71d5826f8f6e0109010c7df7e194f7ca28bf90f2
Gus Cunningham <67352677+cunninghamge@users.noreply.github.com>
2021-09-18 14:00:45 -0600

    add main helper functions with tests

--- main.go
+++ main.go
@@ -1,9 +1,22 @@
 package main
 
 import (
+	"fmt"
 	"os"
 )
 
+func arg(args []string) string {
+	if len(args) > 1 {
+		return args[1]
+	}
+	return ""
+}
+
+func exitWithError(err error) {
+	fmt.Printf("ERROR: %v\n", err)
+	os.Exit(1)
+}
+
 func main() {
-	Start(os.Args[1])
+	newGame(arg(os.Args), os.Stdin, os.Stdout).play()
 }
--- main_test.go
+++ main_test.go
@@ -1,7 +1,9 @@
 package main
 
 import (
+	"errors"
 	"os"
+	"os/exec"
 	"testing"
 )
 
@@ -11,64 +13,107 @@ var (
 )
 
 func TestMain(m *testing.M) {
-	card1 := Card{"diamond", "Queen", 12}
-	card2 := Card{"spade", "3", 3}
-	card3 := Card{"heart", "Ace", 14}
-	testCards = []Card{card1, card2, card3}
-	testDeck = Deck{testCards}
+	testCards = []Card{
+		{"diamond", "Queen", 12},
+		{"spade", "3", 3},
+		{"heart", "Ace", 14},
+	}
+	testDeck = Deck{testCards[:3]}
 
 	code := m.Run()
 	os.Exit(code)
 }
 
-func TestNewDeck(t *testing.T) {
-	newDeck := NewDeck()
-
-	if len(newDeck) != 52 {
-		t.Errorf("got %d not 52 for deck length", len(newDeck))
+func TestArg(t *testing.T) {
+	testCases := map[string]struct {
+		args   []string
+		result string
+	}{
+		"with no filepath": {
+			args:   []string{"exec"},
+			result: "",
+		},
+		"with a filepath": {
+			args:   []string{"exec", "file.txt"},
+			result: "file.txt",
+		},
+		"with an extra arg": {
+			args:   []string{"exec", "file.txt", "extra"},
+			result: "file.txt",
+		},
 	}
 
-	var hearts []Card
-	var diamonds []Card
-	var spades []Card
-	var clubs []Card
-	for _, card := range newDeck {
-		switch card.Suit {
-		case "heart":
-			hearts = append(hearts, card)
-		case "diamond":
-			diamonds = append(diamonds, card)
-		case "spade":
-			spades = append(spades, card)
-		case "club":
-			clubs = append(clubs, card)
-		}
-	}
-
-	for _, suit := range [][]Card{hearts, diamonds, spades, clubs} {
-		if len(suit) != 13 {
-			t.Errorf("got %d not 13 for cards in suit", len(suit))
-		}
+	for name, tc := range testCases {
+		t.Run(name, func(t *testing.T) {
+			got := arg(tc.args)
+			if got != tc.result {
+				t.Errorf("got %s want %s", got, tc.result)
+			}
+		})
 	}
 }
 
-func TestNewPlayers(t *testing.T) {
-	cards := NewDeck()
-	player1, player2 := NewPlayers(cards)
-
-	if player1.Name != "Cacco" {
-		t.Errorf("got %q want %q for player1 name", player1.Name, "Cacco")
+func TestExitWithError(t *testing.T) {
+	if os.Getenv("OS_EXIT_CALLED") == "1" {
+		exitWithError(errors.New("error"))
+		return
 	}
-
-	if player2.Name != "Pickles" {
-		t.Errorf("got %q want %q for player2 name", player2.Name, "Pickles")
-	}
-
-	if len(player1.Deck.Cards) != 26 {
-		t.Errorf("got %d not 26 for player1's cards", len(player1.Deck.Cards))
-	}
-
-	if len(player2.Deck.Cards) != 26 {
-		t.Errorf("got %d not 26 for player2's cards", len(player2.Deck.Cards))
+	subTest := exec.Command(os.Args[0], "-test.run=TestExitWithError")
+	subTest.Env = append(os.Environ(), "OS_EXIT_CALLED=1")
+	err := subTest.Run()
+	if exitError, ok := err.(*exec.ExitError); !ok || exitError.Success() {
+		t.Error("process exited with no error, wanted exit status 1")
 	}
 }
+
+// func TestNewDeck(t *testing.T) {
+// 	newDeck := NewDeck()
+
+// 	if len(newDeck) != 52 {
+// 		t.Errorf("got %d not 52 for deck length", len(newDeck))
+// 	}
+
+// 	var hearts []Card
+// 	var diamonds []Card
+// 	var spades []Card
+// 	var clubs []Card
+// 	for _, card := range newDeck {
+// 		switch card.Suit {
+// 		case "heart":
+// 			hearts = append(hearts, card)
+// 		case "diamond":
+// 			diamonds = append(diamonds, card)
+// 		case "spade":
+// 			spades = append(spades, card)
+// 		case "club":
+// 			clubs = append(clubs, card)
+// 		}
+// 	}
+
+// 	for _, suit := range [][]Card{hearts, diamonds, spades, clubs} {
+// 		if len(suit) != 13 {
+// 			t.Errorf("got %d not 13 for cards in suit", len(suit))
+// 		}
+// 	}
+// }
+
+// func TestNewPlayers(t *testing.T) {
+// 	cards := NewDeck()
+// 	player1, player2 := NewPlayers(cards)
+
+// 	if player1.Name != "Cacco" {
+// 		t.Errorf("got %q want %q for player1 name", player1.Name, "Cacco")
+// 	}
+
+// 	if player2.Name != "Pickles" {
+// 		t.Errorf("got %q want %q for player2 name", player2.Name, "Pickles")
+// 	}
+
+// 	if len(player1.Deck.Cards) != 26 {
+// 		t.Errorf("got %d not 26 for player1's cards", len(player1.Deck.Cards))
+// 	}
+
+// 	if len(player2.Deck.Cards) != 26 {
+// 		t.Errorf("got %d not 26 for player2's cards", len(player2.Deck.Cards))
+// 	}
+// }
